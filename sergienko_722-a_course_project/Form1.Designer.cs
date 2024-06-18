55515cc8f86a79bfc148c6e7c63fb5700b6ed52e
Владислав Сергієнко <Vladyslav.Serhiienko@infiz.khpi.edu.ua>
2024-06-18 00:41:44 +0000

    Finish Task number 1

--- sergienko_722-a_course_project/Form1.Designer.cs
+++ sergienko_722-a_course_project/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            bStart = new Button();
+            tblnput = new TextBox();
+            tClock = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(341, 134);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // bStart
+            // 
+            bStart.Location = new Point(316, 266);
+            bStart.Name = "bStart";
+            bStart.Size = new Size(75, 23);
+            bStart.TabIndex = 1;
+            bStart.Text = "Пуск";
+            bStart.UseVisualStyleBackColor = true;
+            bStart.Click += bStart_Click;
+            // 
+            // tblnput
+            // 
+            tblnput.Enabled = false;
+            tblnput.Location = new Point(272, 186);
+            tblnput.Name = "tblnput";
+            tblnput.Size = new Size(174, 23);
+            tblnput.TabIndex = 2;
+            tblnput.KeyPress += tblnput_KeyPress;
+            // 
+            // tClock
+            // 
+            tClock.Interval = 25000;
+            tClock.Tick += tClock_Tick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tblnput);
+            Controls.Add(bStart);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button bStart;
+        private TextBox tblnput;
+        private System.Windows.Forms.Timer tClock;
     }
 }
--- sergienko_722-a_course_project/Form1.cs
+++ sergienko_722-a_course_project/Form1.cs
@@ -2,9 +2,58 @@ namespace sergienko_722_a_course_project
 {
     public partial class Form1 : Form
     {
+        private bool Mode;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void tClock_Tick(object sender, EventArgs e)
+        {
+            tClock.Stop();
+            MessageBox.Show("Минуло 25 секунд", "Увага");
+            tClock.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Mode = true;
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            if (Mode)
+            {
+                tblnput.Enabled = true;
+                tblnput.Focus();
+                tClock.Start();
+                bStart.Text = "Стоп"; // зміна тексту на кнопці на "Стоп"
+                this.Mode = false;
+            }
+            else
+            {
+                tblnput.Enabled = false;
+                tClock.Stop();
+                bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
+                this.Mode = true;
+            }
+        }
+
+        private void tblnput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            tClock.Stop();
+            tClock.Start();
+            if ((e.KeyChar >= '0') & (e.KeyChar <= '9') | (e.KeyChar == (char)8))
+            {
+                return;
+            }
+            else
+            {
+                tClock.Stop();
+                MessageBox.Show("Неправильний символ", "Помилка");
+                tClock.Start();
+                e.KeyChar = (char)0;
+            }
         }
     }
 }
