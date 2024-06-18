0a35d757bf0f50de7d819d353426b1e1e41cbd1e
Владислав Сергієнко <Vladyslav.Serhiienko@infiz.khpi.edu.ua>
2024-06-18 00:30:02 +0000

    Добавьте файлы проекта.

---
+++ sergienko_722-a_course_project/Form1.cs
@@ -0,0 +1,10 @@
+namespace sergienko_722_a_course_project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
